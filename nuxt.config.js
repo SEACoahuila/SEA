a85f37153ce796e115feef57361db1cbef1406e5
rene lazalde <rene.lazalde@gmail.com>
2023-05-01 16:32:47 -0600

    Cambios para establecer pagina web

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/SEA/'
+    base: '/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
