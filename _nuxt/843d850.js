32ac5f3eaa57f4106c478c0287750b3dc4ce1909
Renelzm <Renelzm@users.noreply.github.com>
2023-05-01 21:08:09 +0000

    deploy: 49cc2bbb5619697bc2a24f75d6373e951322a979

--- _nuxt/843d850.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see LICENSES */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{807:function(e,n,r){"use strict";r.r(n);var o=r(642);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.20.0","app-compat")}}]);
