a19cc2f6c85e6880a51c63b85db8d0b6e8a535a1
rene lazalde <rene.lazalde@gmail.com>
2023-05-01 18:11:03 -0600

    Cambios para establecer pagina web

---
+++ pages/secretaria-ejecutiva/publicaciones/peac.pdf/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Peac',
+    computed: {
+        redireccionarPeac() {
+            window.location.assign('peac/peac.pdf')
+        }
+    },
+    beforeMount() {
+       this.redireccionarPeac()
+    },
+    
+    
+    
+    }
+</script>
+<style lang="scss" scoped>
+
+</style>
