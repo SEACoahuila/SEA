226727123e6cb22ddcad1e9718668f03ee5383e3
rene lazalde <rene.lazalde@gmail.com>
2023-05-14 21:17:37 -0600

    Se suben cambios por errores de juridico

---
+++ pages/secretaria-ejecutiva/transparencia/art-21-03-directorio.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Redireccionando...</h1>
+    </template>
+    
+    <script>
+    export default {
+  
+      mounted() {
+      
+          this.$router.push('/transparencia/21-03/')
+     
+  
+        }
+    }
+    </script>
+    
+    <style scoped>
+  
+    </style>
+    
