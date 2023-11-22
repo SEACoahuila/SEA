aa5a9d3c5d499e25458c586a19628884d2cb1e64
Renelzm <Renelzm@users.noreply.github.com>
2023-11-22 16:01:15 +0000

    deploy: d40022e154b616025cba8f87339d61680390e283

--- _nuxt/13c13e5.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see LICENSES */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{826:function(e,n,r){"use strict";r.r(n);var o=r(651);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.23.0","app-compat")}}]);
---
+++ _nuxt/static/1700668831/publicaciones/dibuja-tus-valores/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/publicaciones/dibuja-tus-valores", {data:[{}],fetch:{},mutations:[]});
