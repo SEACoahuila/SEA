ae9b96ddce6b2b8f37f13d1f6a00ec4b06605b19
rene lazalde <rene.lazalde@gmail.com>
2024-01-12 12:28:06 -0600

    cambios en titularidad SEFIRC

--- pages/cc/index.vue
+++ pages/cc/index.vue
@@ -399,34 +399,35 @@ export default {
                 },
 
                 {
-                    nombre: 'TERESA GUAJARDO BERLANGA',
+                    nombre: 'ELMA MARISOL MARTÍNEZ GONZÁLEZ',
                     cargo: 'Secretaria de Fiscalización y Rendición de Cuentas del Estado de Coahuila de Zaragoza.',
-                    foto: 'cc-sefirc-tgb.jpg',
+                    foto: 'cc-sefirc-emm.jpg',
                     img: 'cc-sefirc.png',
                     web: 'http://www.sefircoahuila.gob.mx/',
                     curriculum: [{
                             subtitulo: 'Formación Académica',
                             lista: [
-                                'Es Licenciada en Derecho por la Facultad de Jurisprudencia de la Universidad Autónoma de Coahuila.'
+                                'Es licenciada en Derecho egresada de la Facultad de Jurisprudencia de la Universidad Autónoma de Coahuila, especializada en Derecho a la Información, Fiscalización y Combate a la Corrupción por la Universidad Interamericana de Derechos Humanos, con estudios de maestría en Impuestos por la Universidad Autónoma del Noreste.'
                             ]
                         },
                         {
-                            subtitulo: 'Cuenta con diversos diplomados, tales como:',
+                            subtitulo: 'Experiencia Profesional:',
                             lista: [
-                                'Diplomado en Transparencia y Buen Gobierno por el Instituto Tecnológico de Estudios Superiores de Monterrey.',
-                                'Diplomado en Políticas Públicas, Gerencia y Gobierno Local por el Colegio Nacional de Ciencias Políticas y Administración Pública A.C.',
-                                'Diplomado en materia de Derechos Humanos por la Universidad Autónoma de Coahuila, Comisión Nacional de Derechos Humanos y Comisión de Derechos Humanos del Estado de Coahuila Cuenta con certificación en materia de Protección de Datos Personales en posesión de los particulares por el Instituto Nacional de Acceso a la Información.',
-                                'Diplomado Virtual de Contabilidad Gubernamental por la Asociación Nacional de Organismos de Fiscalización Superior y Control Gubernamental A.C.'
+                                'Coordinadora Jurídica de la Tesorería y como Coordinadora de Responsabilidades y Asistencia Jurídica. Presidencia Municipal de Saltillo.',
+                                'Subdirectora en la Coordinación de Asuntos Jurídicos.  Auditoría Superior del Estado.',
+                                'Directora de Seguimiento de Procedimientos. Secretaría de Fiscalización y Rendición de Cuentas.',
+                                'Coordinadora y Facilitador Jurídico del “Proyecto de Modernización Normativa de la Secretaría de Seguridad Pública del Estado de Coahuila”. Secretaría de la Contraloría y Modernización Administrativa.',
+                                'Asesor Jurídico en el Voluntariado de Coahuila y Abogado Defensor. Procuraduría de la Familia del Sistema DIF Coahuila.'
                             ]
                         },
                         {
-                            subtitulo: 'Otras Actividades Profesionales',
+                            subtitulo: 'diplomados y cursos',
                             lista: [
-                                'Fue Presidenta del Instituto Coahuilense de Acceso a la Información Pública; del cual formó parte activa durante 13 años.',
-                                'Coordinó la Comisión de Educación de la COMAIP, impulso una nueva Ley de Acceso a la Información Pública y Protección de Datos Personales ante el Congreso del Estado de Coahuila de Zaragoza, convirtiéndose nuestro Estado y su Legislación referentes a nivel nacional en la materia.',
-                                'Dentro del Sistema Nacional de Transparencia ocupo la Secretaria Técnica de la Comisión de los Derechos Humanos, Equidad de Género e Inclusión Social.',
-                                'En el aspecto académico ha desempeñado actividades de docencia en educación universitaria de Instituciones Públicas y Privadas de la Región Carbonífera y de la Capital del Estado de Coahuila.',
-                                'Es Conferencista en materia de Transparencia, Acceso a la Información, Rendición de Cuentas y Protección de Datos Personales y ha escrito diversos artículos publicados en un medio de comunicación.'
+                                'Normas Profesionales de Auditoría del Sistema Nacional de Fiscalización',
+                                'Ley General de Responsabilidades Administrativas',
+                                'Derecho Municipal',
+                                'Derecho Laboral Burocrático,',
+                                'Seminario Nacional de Vinculación de los Sistemas Nacionales Anticorrupción, Fiscalización y Transparencia.'
                             ]
                         },
 
@@ -461,7 +462,7 @@ export default {
                             ]
                         },
                         {
-                            subtitulo: 'DIPLOMADOS Y CURSOS',
+                            subtitulo: 'diplomados y cursos',
                             lista: [                      
                             'Curso de Actualización en Materia Jurídica 2022 • Coordinación de Posgrado Facultad de Jurisprudencia de la UAdeC.',
                             'Curso de Gobierno Abierto 2022 • Programa de las Naciones Unidas para el Desarrollo Instituto Nacional de Transparencia, Acceso a la Información Y Protección de Datos Personales.',
