43cd649da0e26cc7dd45e6939537ca764f3fb07a
rene lazalde <rene.lazalde@gmail.com>
2024-02-07 10:12:09 -0600

    Prueba 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vuex-persist', ssr: false },
+    // { src: '~/plugins/vuex-persist', ssr: false },
     { src: '~/plugins/vue-uuid.js', ssr: false},
     { src: '~/plugins/vue-gtag.js', ssr: false}
     
