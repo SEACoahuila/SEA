c4757a1fa074d58ab43f22e3ebd8578fec60ef60
dluna88-sea <dluna88-sea@users.noreply.github.com>
2024-08-20 18:20:59 +0000

    deploy: b7f6c977b07082a263e42dec73416b02984489bc

--- _nuxt/7565164.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[39],{903:function(n,t,e){"use strict";e.r(t);var o={mounted:function(){this.$router.push("/transparencia/21-25/")}},c=e(69),component=Object(c.a)(o,(function(){return(0,this._self._c)("h1",[this._v("Redireccionando...")])}),[],!1,null,"429a2ddb",null);t.default=component.exports}}]);
